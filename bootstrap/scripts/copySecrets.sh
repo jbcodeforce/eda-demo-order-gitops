35eeb85f6ea64068fb36053278f42e82ca57de7c
Jerome Boyer <jerome.boyer@gmail.com>
2021-12-21 08:19:31 -0800

    update tls for security

--- bootstrap/scripts/copySecrets.sh
+++ bootstrap/scripts/copySecrets.sh
@@ -1,3 +1,15 @@
-YOUR_PROJECT_NAME=$1
-oc project $YOUR_PROJECT_NAME
-oc get secret ibm-entitlement-key -n ibm-common-services -o json | jq -r '.metadata.namespace="'${YOUR_PROJECT_NAME}'"' | oc apply -f -
+
+set -x
+if [[ $# -ne 3 ]] 
+then
+	echo "parameters: secret_name source_namespace target_namespace"
+else
+    NSSRC=$2
+	NSTGT=$3
+	NAME=$1
+	echo "create secret $NAME from $NSSRC to $NSTGT"
+	oc get secret $1 --namespace=$NSSRC -o json \
+	| jq  'del(.metadata.uid, .metadata.selfLink, .metadata.creationTimestamp, .metadata.ownerReferences)' \
+	| jq -r '.metadata.namespace="'${NSTGT}'"' \
+	| oc apply --namespace=$NSTGT -f -
+fi
